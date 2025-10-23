04e62f0ccf4d3cd40f92f8c30c42b1c7b906de06
Karan Mehta <karanmehtawork9@gmail.com>
2025-10-23 17:32:13 +0100

    made pdf to image conversion work

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,36 +1,63 @@
-import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
+import Navbar from "~/components/Navbar";
 import ResumeCard from "~/components/ResumeCard";
-import { resumes } from "~/constants";
-import { resume } from "react-dom/server";
 import {usePuterStore} from "~/lib/puter";
 import {Link, useNavigate} from "react-router";
 import {useEffect, useState} from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "Resume Mind" },
-    { name: "description", content: "Smart Feedback for your Dream Job" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
   const { auth, kv } = usePuterStore();
   const navigate = useNavigate();
+  const [resumes, setResumes] = useState<Resume[]>([]);
+  const [loadingResumes, setLoadingResumes] = useState(false);
+
   useEffect(() => {
     if(!auth.isAuthenticated) navigate('/auth?next=/');
   }, [auth.isAuthenticated])
 
-  
+  useEffect(() => {
+    const loadResumes = async () => {
+      setLoadingResumes(true);
+
+      const resumes = (await kv.list('resume:*', true)) as KVItem[];
+
+      const parsedResumes = resumes?.map((resume) => (
+          JSON.parse(resume.value) as Resume
+      ))
+
+      setResumes(parsedResumes || []);
+      setLoadingResumes(false);
+    }
+
+    loadResumes()
+  }, []);
+
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
-      
-      <Navbar />
-      <section className="main-section">
+    <Navbar />
+
+    <section className="main-section">
       <div className="page-heading py-16">
         <h1>Track Your Applications & Resume Ratings</h1>
-        <h2>Review your submissions and check AI-powered feedback.</h2>
+        {!loadingResumes && resumes?.length === 0 ? (
+            <h2>No resumes found. Upload your first resume to get feedback.</h2>
+        ): (
+          <h2>Review your submissions and check AI-powered feedback.</h2>
+        )}
       </div>
-      {resumes.length > 0 && (
+      {loadingResumes && (
+          <div className="flex flex-col items-center justify-center">
+            <img src="/images/resume-scan-2.gif" className="w-[200px]" />
+          </div>
+      )}
+
+      {!loadingResumes && resumes.length > 0 && (
         <div className="resumes-section">
           {resumes.map((resume) => (
               <ResumeCard key={resume.id} resume={resume} />
@@ -38,10 +65,13 @@ export default function Home() {
         </div>
       )}
 
-      </section>
-      
-      
-      
+      {!loadingResumes && resumes?.length === 0 && (
+          <div className="flex flex-col items-center justify-center mt-10 gap-4">
+            <Link to="/upload" className="primary-button w-fit text-xl font-semibold">
+              Upload Resume
+            </Link>
+          </div>
+      )}
+    </section>
   </main>
-
 }
--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -5,7 +5,7 @@ import {usePuterStore} from "~/lib/puter";
 import {useNavigate} from "react-router";
 import {convertPdfToImage} from "~/lib/pdf2img";
 import {generateUUID} from "~/lib/utils";
-import {prepareInstructions} from "~/constants";
+import {AIResponseFormat,prepareInstructions} from "~/constants";
 
 const Upload = () => {
     const { auth, isLoading, fs, ai, kv } = usePuterStore();
@@ -48,7 +48,7 @@ const Upload = () => {
 
         const feedback = await ai.feedback(
             uploadedFile.path,
-            prepareInstructions({ jobTitle, jobDescription, AIResponseFormat: 'json' })
+            prepareInstructions({ jobTitle, jobDescription ,AIResponseFormat })
         )
         if (!feedback) return setStatusText('Error: Failed to analyze resume');
 
@@ -72,6 +72,7 @@ const Upload = () => {
         const companyName = formData.get('company-name') as string;
         const jobTitle = formData.get('job-title') as string;
         const jobDescription = formData.get('job-description') as string;
+        // console.log({ companyName, jobTitle, jobDescription, file });
 
         if(!file) return;
 
