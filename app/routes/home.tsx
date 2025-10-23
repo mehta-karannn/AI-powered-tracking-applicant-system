2066b9d9eca0d56dda4ca81ffc6ed50d858b6e2c
Karan Mehta <karanmehtawork9@gmail.com>
2025-10-23 04:14:58 +0100

    Integrated Puter with authenication

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,6 +3,9 @@ import type { Route } from "./+types/home";
 import ResumeCard from "~/components/ResumeCard";
 import { resumes } from "~/constants";
 import { resume } from "react-dom/server";
+import {usePuterStore} from "~/lib/puter";
+import {Link, useNavigate} from "react-router";
+import {useEffect, useState} from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -12,6 +15,12 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const { auth, kv } = usePuterStore();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if(!auth.isAuthenticated) navigate('/auth?next=/');
+  }, [auth.isAuthenticated])
+
   
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       
