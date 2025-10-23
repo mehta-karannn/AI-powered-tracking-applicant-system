a0aaa96f32c2ad4d787dfaebdc88a5d5c9d27742
Karan Mehta <karanmehtawork9@gmail.com>
2025-10-23 02:41:04 +0100

    aDDED nAVBAR COMPONENT IN THE HOME PAGE

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,3 +1,4 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
 
 
@@ -9,13 +10,16 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
       <section className="main-section">
       <div className="page-heading py-16">
         <h1>Track Your Applications & Resume Ratings</h1>
         <h2>Review your submissions and check AI-powered feedback.</h2>
       </div>
       </section>
+      {}
   </main>
 
 }
