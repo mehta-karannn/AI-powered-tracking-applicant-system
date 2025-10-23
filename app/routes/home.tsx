2eda5d3cae1170b4924c339431aba294bc68dc29
Karan Mehta <karanmehtawork9@gmail.com>
2025-10-23 03:42:23 +0100

    Added cards section in home page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,8 @@
 import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-
+import ResumeCard from "~/components/ResumeCard";
+import { resumes } from "~/constants";
+import { resume } from "react-dom/server";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -12,14 +14,25 @@ export function meta({}: Route.MetaArgs) {
 export default function Home() {
   
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      
       <Navbar />
       <section className="main-section">
       <div className="page-heading py-16">
         <h1>Track Your Applications & Resume Ratings</h1>
         <h2>Review your submissions and check AI-powered feedback.</h2>
       </div>
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
+
       </section>
-      {}
+      
+      
+      
   </main>
 
 }
